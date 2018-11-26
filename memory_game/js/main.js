08677aecd8f749c409432fc8f3bba734f83186a9
idothestamping <douglas.klemp@gmail.com>
2018-11-26 00:14:56 -0800

    Added Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,24 @@
-console.log("Up and running!");
-
+/*console.log("Up and running!");
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []
+var cardOne = cards[1];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] != cardsInPlay[1]) {
+	alert("Sorry, try again!");
+}
+
+
